2b525d9dd0cbc611b142286c8804279517430058
tapsa <tapsa872@gmail.com>
2018-02-08 18:35:09 +0200

    Basic UI structure made

--- emp-register-app/src/app/employees/shared/employee.service.ts
+++ emp-register-app/src/app/employees/shared/employee.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@angular/core';
 
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
+import { Employee } from "./employee.model";
+
 @Injectable()
 export class EmployeeService {
 
-  constructor() { }
+  employeelist: AngularFireList<any>;
+  selectedEmployee: Employee = new Employee();
+
+  constructor(private _firebase: AngularFireDatabase) { }
+
+  getData() {
+    this.employeelist = this._firebase.list("employees");
+    return this.employeelist;
+  }
+
+  insertEmployee(employee: Employee) {
+    this.employeelist.push({
+      name: employee.name,
+      position: employee.position,
+      office: employee.office,
+      salary: employee.salary
+    });
+  }
+
+  updateEmployee(employee: Employee) {
+    this.employeelist.update(employee.$key, {
+      name: employee.name,
+      position: employee.position,
+      office: employee.office,
+      salary: employee.salary
+    });
+  }
+
+  deleteEmployee($key: string) {
+    this.employeelist.remove($key);
+  }
 
 }

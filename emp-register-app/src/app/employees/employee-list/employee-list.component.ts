bd403a35381f8bd9ec9c1494fe38fb26fb0f58ed
tapsa <tapsa872@gmail.com>
2018-02-09 07:26:06 +0200

    list employees to the page

--- emp-register-app/src/app/employees/employee-list/employee-list.component.ts
+++ emp-register-app/src/app/employees/employee-list/employee-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import{ EmployeeService } from "../shared/employee.service";
+import { Employee } from "../shared/employee.model";
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-employee-list',
@@ -9,9 +11,20 @@ import{ EmployeeService } from "../shared/employee.service";
 })
 export class EmployeeListComponent implements OnInit {
 
+  employeeList: Employee[];
+
   constructor(private _employeeService: EmployeeService) { }
 
   ngOnInit() {
+    var empData = this._employeeService.getData();
+    empData.snapshotChanges().subscribe(item => {
+      this.employeeList = [];
+      item.forEach(element => {
+        var storageElement = element.payload.toJSON();
+        storageElement["$key"] = element.key;
+        this.employeeList.push(storageElement as Employee);
+      });
+    });
   }
 
 }

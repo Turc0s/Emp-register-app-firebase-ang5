8e8e1c70f7c14af51ad1ab31ffec7a6ea372ff6b
tapsa <tapsa872@gmail.com>
2018-02-09 07:50:53 +0200

    Added delete record functionality

--- emp-register-app/src/app/employees/employee-list/employee-list.component.ts
+++ emp-register-app/src/app/employees/employee-list/employee-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import{ EmployeeService } from "../shared/employee.service";
 import { Employee } from "../shared/employee.model";
-import { element } from 'protractor';
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: 'app-employee-list',
@@ -13,7 +13,8 @@ export class EmployeeListComponent implements OnInit {
 
   employeeList: Employee[];
 
-  constructor(private _employeeService: EmployeeService) { }
+  constructor(private _employeeService: EmployeeService,
+              private _toastrService: ToastrService) { }
 
   ngOnInit() {
     var empData = this._employeeService.getData();
@@ -28,11 +29,16 @@ export class EmployeeListComponent implements OnInit {
   }
 
   onEdit(emp: Employee) {
-
     // this._employeeService.selectedEmployee = emp;
     this._employeeService.selectedEmployee = Object.assign({}, emp);
+  }
 
+  onDelete(key: string) {
 
+    if(confirm("Are you sure to delete this record ?") == true) {
+      this._employeeService.deleteEmployee(key);
+      this._toastrService.warning("Deleted Successfully", "Employee Register");
+    }
   }
 
 }
